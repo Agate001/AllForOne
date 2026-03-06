e9450d5d1ff7df03b7a0e3c41259c4e421cdc308
Carlos <carlos.lopez5639@gmail.com>
2026-03-06 07:22:19 -0800

    greaterthanlesssthan better response

--- services/Greaterlessthan.cs
+++ services/Greaterlessthan.cs
@@ -6,14 +6,14 @@ namespace AllForOne.services
         {
             if (num1 > num2)
             {
-                return $"{num1} is greater than {num2}. {num2} is less than {num1}.";
+                return $"{num1} is greater than {num2}.";
             }
             else if (num1 < num2)
             {
-                return $"{num1} is less than {num2}. {num2} is greater than {num1}.";
+                return $"{num1} is less than {num2}.";
             }
 
-            return $"{num1} is equal to {num2}. {num2} is equal to {num1}.";
+            return $"{num1} is equal to {num2}.";
         }
     }
 }
